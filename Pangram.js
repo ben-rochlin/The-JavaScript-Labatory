b0ec7b15d69fd6001ce7322bb9e676c7d11c510b
ben-rochlin <63221950+ben-rochlin@users.noreply.github.com>
2021-03-11 23:07:33 +1100

    Create Pangram.js

---
+++ Pangram.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant). Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+const string = "The quick brown fox ju over the lazy dog."
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+const process = (str) => {
+
+  
+    for ( let letter of alphabet.split('')){
+        console.log(letter)
+        if (str.includes(letter) === false){
+            return false
+            continue
+        }
+        else return true
+    }
+}
+
+console.log(process(string))
